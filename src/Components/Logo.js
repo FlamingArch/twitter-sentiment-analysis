3151cb07824ef7cb2a625ef42e620910a42482ae
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-10-06 05:43:53 +0530

    Added Image to TopBar that blurs on collapse

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div>
-      <p style={{ color: "white", width: "100px", lineHeight: 1 }}>
+      <p style={{ color: "white", width: "100px", lineHeight: 1, zIndex: 2 }}>
         Twitter Sentiment Analysis
       </p>
     </div>
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Logo from "./Logo";
 import SearchBox from "./SearchBox";
+import bg from "../Resources/bg.jpg";
 
 const TopBar = () => {
   const [expanded, setExpanded] = useState(true);
 
   const containerTailwindStyles =
-    "w-full bg-accent1 text-xl text-white p-12 flex flex-col place-content-center shadow-xl";
+    "w-full bg-accent1 text-xl text-white flex flex-col place-content-center shadow-xl relative";
 
   const onTextChange = (val) => {
     val === "" ? setExpanded(true) : setExpanded(false);
@@ -15,15 +16,37 @@ const TopBar = () => {
 
   return (
     <motion.div
-      animate={expanded ? { height: "100%" } : { height: "auto" }}
+      animate={expanded ? { height: "100%" } : { height: "240px" }}
       className={`${containerTailwindStyles}`}
+      style={{
+        backgroundImage: `url('https://unsplash.com/photos/FdDkfYFHqe4/download?force=true')`,
+        backgroundPosition: "50% 50%",
+        backgroundSize: "cover",
+      }}
     >
-      <Logo />
-      <div className="h-full grid place-items-center">
-        <SearchBox onChange={onTextChange} />
-      </div>
+      <motion.div
+        animate={
+          expanded
+            ? {
+                backgroundColor: "#0006",
+              }
+            : {
+                backgroundColor: "#000C",
+                backdropFilter: "blur(2rem)",
+                WebkitBackdropFilter: "blur(2rem)",
+              }
+        }
+        className="w-full h-full p-12"
+      >
+        <Logo />
+        <div className="h-full grid place-items-center">
+          <SearchBox onChange={onTextChange} />
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
 
 export default TopBar;
+
+// https://unsplash.com/photos/FdDkfYFHqe4/download?force=true
