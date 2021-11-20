6da3fb1eeaebb2adfe977f1ce80ac1284a48c45a
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-11-21 00:06:03 +0530

    Update get_tweets.py

--- src/scripts/TwitterAbstractions/get_tweets.py
+++ src/scripts/TwitterAbstractions/get_tweets.py
@@ -1,5 +1,3 @@
-
-
 from tweepy.api import API
 from authentication import authenticate
 
@@ -8,5 +6,14 @@ def get_home_tweets(screen_name, count=20, api=API(authenticate())):
     """
     Get the user's home timeline.
     """
-    tweets = api.user_timeline(screen_name=screen_name, count=count, tweet_mode="extended")
+    tweets = api.user_timeline(
+        screen_name=screen_name, count=count, tweet_mode="extended")
+    return tweets
+
+
+def get_hashtag_tweets(hashtag, count=20, api=API(authenticate())):
+    """
+    Get the tweets with the given hashtag.
+    """
+    tweets = api.search(q=hashtag, count=count, tweet_mode="extended")
     return tweets
